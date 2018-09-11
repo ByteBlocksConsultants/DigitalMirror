70b1d37edc91da4ce01cd90327d774608a245de2
Angelina Ilieva <kolosi98@hotmail.com>
2018-09-10 22:54:43 -0600

    fix controller

---
+++ BB.DigitalMirror.Business/Interfaces/IContractRepository.cs
@@ -0,0 +1,15 @@
+﻿using BB.DigitalMirror.API.Controllers;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BB.DigitalMirror.Business
+{
+    public interface IContractRepository
+    {
+       Task<List<Contract>> GetContracts();
+
+       Task<Contract> GetContractById(int id);
+
+       Task<Contract> CreateContract(ContractRequest request);
+    }
+}
